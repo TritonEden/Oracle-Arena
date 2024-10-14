fdd7376d2d931cd92cbf08cd6d4282ce31717d4b
Shm00py <willwalterduff@icloud.com>
2024-10-14 17:56:13 -0400

    Uploading frontend project

---
+++ frontend/navbar/navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './navbar.module.css';
+
+const Navbar: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navContainer}>
+                <Link href="home">
+                    <Image
+                        src="/images/LogoWithText.png"
+                        width={300}
+                        height={100}
+                        alt="logo"
+                    />
+                </Link>
+
+                {/* Hamburger Menu Icon */}
+                <button className={styles.menuButton} onClick={toggleMenu} aria-label="Toggle menu">
+                    ☰
+                </button>
+
+                {/* Navigation Links */}
+                <ul className={`${styles.navLinks} ${menuOpen ? styles.active : ''}`}>
+                    <li>
+                        <Link href="home">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="statistics">Stats</Link>
+                    </li>
+                    <li>
+                        <Link href="betPredict">Bet Predictions</Link>
+                    </li>
+                    <li>
+                        <Link href="about">About</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
