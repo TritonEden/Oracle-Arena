9c4d12c25775ff4ad4e34782ce4ac53ad95bfd67
Shm00py <willwalterduff@icloud.com>
2025-02-11 12:46:41 -0500

    Continued work on issue #5

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -19,23 +19,36 @@ export default function Home() {
         </div>
       </div>
 
+      {/* 
+        - Div table to display game schedule
+        - This method was used for styling purposes
+      */}
       <div className={styles.tableContainer}>
-        <table className={styles.gamesTable}>
-          <thead>
-            <tr>
-              <th>Logo A</th>
-              <th>Name A</th>
-              <th>VS.</th>
-              <th>Name B</th>
-              <th>Logo B</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Welp</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className={styles.gamesTable}>
+          <div className={styles.headerRow}>
+            <div>Visiting Team</div>
+            <div>at</div>
+            <div>Home Team</div>
+          </div>
+          {/* Table Body */}
+          <div className={styles.gameRow}>
+            <div className={styles.tableCell}>Logo 1</div>
+            <div className={styles.tableCell}>Team 1</div>
+            <div className={styles.tableCell}>Start Time</div>
+            <div className={styles.tableCell}>Team 2</div>
+            <div className={styles.tableCell}>Logo 2</div>
+          </div>
+          <div className={styles.dataLabelRow}>
+            <div className={styles.tableCell}>Winner (Prediction vs Actual)</div>
+            <div className={styles.tableCell}>Total Score (Prediction vs Actual)</div>
+          </div>
+          <div className={styles.dataRow}>
+            <div className={`${styles.tableCell} ${styles.actualData}`}>Pred. Winner</div>
+            <div className={`${styles.tableCell} ${styles.predictData}`}>Act. Winner</div>
+            <div className={`${styles.tableCell} ${styles.actualData}`}>Pred. Total</div>
+            <div className={`${styles.tableCell} ${styles.predictData}`}>Act. Total</div>
+          </div>
+        </div>
       </div>
 
     </main>
