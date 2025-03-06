d7b206a708d405dd12b0c2343999c26849870c9a
Shm00py <willwalterduff@icloud.com>
2025-03-06 15:25:37 -0500

    #19, continued setup, #20 subtab complete

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -4,42 +4,42 @@ import { useState } from 'react'
 import styles from "./page.module.css";
 
 const about = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [activeSection, setActiveSection] = useState<string | null>(null);
 
   return (
     <div className={styles.pageContainer}>
       <div className={styles.pageNav}>
         <button
-          // className={`${styles.searchOverlay} ${!isActive ? styles.searchOverlayActive : ''}`}
-          // onClick={showAbout}
+          className={`${styles.navItem} ${activeSection === "about" ? styles.navItemActive : ''}`}
+          onClick={() => setActiveSection("about")}
           aria-label="Show About Sub-Page"
         >
           About the Team
         </button>
         <button
-          // className={`${styles.searchOverlay} ${!isActive ? styles.searchOverlayActive : ''}`}
-          // onClick={showAbout}
+          className={`${styles.navItem} ${activeSection === "model" ? styles.navItemActive : ''}`}
+          onClick={() => setActiveSection("model")}
           aria-label="Show Model Explanation Sub-Page"
         >
           Model Explanation
         </button>
         <button
-          // className={`${styles.searchOverlay} ${!isActive ? styles.searchOverlayActive : ''}`}
-          // onClick={showAbout}
+          className={`${styles.navItem} ${activeSection === "use" ? styles.navItemActive : ''}`}
+          onClick={() => setActiveSection("use")}
           aria-label="Show How to Use Data Visualization Sub-Page"
         >
           How to Use Data Visualization
         </button>
       </div>
 
-      <div className="about">
-        ...
+      <div className={`${styles.about} ${styles.subPage}`} style={{ display: activeSection === "about" ? "block" : "none" }}>
+        This is the section that is about the team.
       </div>
-      <div className={styles.modelExplain}>
-        ...
+      <div className={`${styles.modelExplain} ${styles.subPage}`} style={{ display: activeSection === "model" ? "block" : "none" }}>
+        This is the section that explains how the prediction model works.
       </div>
-      <div className={styles.useExplain}>
-        ...
+      <div className={`${styles.useExplain} ${styles.subPage}`} style={{ display: activeSection === "use" ? "block" : "none" }}>
+        This is the section that explains how to use the data visualizations.
       </div>
     </div>
   )
