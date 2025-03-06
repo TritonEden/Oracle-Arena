90174b800f12c20db4c7261516a273325ee0eeeb
Shm00py <willwalterduff@icloud.com>
2025-03-06 13:58:22 -0500

    Setting up About page, #19

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,12 +1,46 @@
-import React from 'react'
+'use client';
+
+import { useState } from 'react'
+import styles from "./page.module.css";
 
 const about = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <div>
-        <h1 style={{ marginTop: '300px', textAlign: 'center' }}>ABOUT</h1>
-        <h2>
-          Coming Soon <span className="loadingDots"></span>
-        </h2>
+    <div className={styles.pageContainer}>
+      <div className={styles.pageNav}>
+        <button
+          // className={`${styles.searchOverlay} ${!isActive ? styles.searchOverlayActive : ''}`}
+          // onClick={showAbout}
+          aria-label="Show About Sub-Page"
+        >
+          About the Team
+        </button>
+        <button
+          // className={`${styles.searchOverlay} ${!isActive ? styles.searchOverlayActive : ''}`}
+          // onClick={showAbout}
+          aria-label="Show Model Explanation Sub-Page"
+        >
+          Model Explanation
+        </button>
+        <button
+          // className={`${styles.searchOverlay} ${!isActive ? styles.searchOverlayActive : ''}`}
+          // onClick={showAbout}
+          aria-label="Show How to Use Data Visualization Sub-Page"
+        >
+          How to Use Data Visualization
+        </button>
+      </div>
+
+      <div className="about">
+        ...
+      </div>
+      <div className={styles.modelExplain}>
+        ...
+      </div>
+      <div className={styles.useExplain}>
+        ...
+      </div>
     </div>
   )
 }
