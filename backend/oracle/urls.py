5cb4e313bba45ea1c42ad3789d60d257b4b0257a
Ryan <yhg461@vols.utk.edu>
2025-04-11 16:17:40 -0400

    Tested queries on api

--- backend/oracle/urls.py
+++ backend/oracle/urls.py
@@ -25,19 +25,17 @@ urlpatterns = [
     #make a path that takes in just a playerid
     # path('playersummary/<int:playerid>/', playersummary),
     path('presentGameSummary/', presentGameSummary),
-    path('api/player_stats/', get_players, name='get_players'),
     # path('api/player_stats/', player_stats, name='player_stats'),
     path("api/player_game_stats/<int:player_id>/", get_player_game_stats, name="player_game_stats"),
     path("api/player_game_stats/", get_all_player_game_stats, name="all_player_game_stats"),
+    path("api/game/<int:game_id>/", get_game, name="game"),
     path("api/games/", get_games, name="games"),
     path("api/teams/", get_teams, name="teams"),
     path("api/players/", get_players, name="players"),
-    path("api/wins_losses/<int:team_id>/<str:season_year>/", get_wins_losses, name="wins_losses"),
+    #path("api/wins_losses/<int:team_id>/<str:season_year>/", get_wins_losses, name="wins_losses"),
     path("api/team_game_results/<int:team_id>/<str:season_year>/", get_team_game_results, name="team_game_results"),
-    path("api/game_ids/", get_game_ids, name="game_ids"),
     path("api/player_stats_for_season/<int:player_id>/<str:season_year>/", get_player_stats_for_season, name="player_stats_for_season"),
     path("api/player_average_stats_for_season/<int:player_id>/<str:season_year>/", get_player_average_stats_for_season, name="player_average_stats_for_season"),
     path("api/players_from_team/<int:team_id>/<str:season_year>/", get_players_from_team, name="players_from_team"),
-    path("api/game/<int:game_id>/", get_game, name="game"),
-    path("api/home_away_team_info_on_date/<str:date>/", get_home_away_team_info_on_date, name="home_away_team_info_on_date")
+    path("api/home_away_team_info_on_date/<str:game_date>/", get_home_away_team_info_on_date, name="home_away_team_info_on_date")
 ]
